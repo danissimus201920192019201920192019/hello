cef0509687e827b3cba204420d7a2cb9784b1756
danissimus201920192019201920192019 <danissimus2013@gmail.com>
2019-10-22 22:21:33 +0500

    commit 4

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,8 +5,19 @@ using namespace std;
 int main()
 {
    cout << "Hello world!" << endl; 
-   int d;
+   char d[100];
+   bool flag = false;
    cin >> d;
-   cout << d;
+   for(int i = 0; d[i] != '\n'; i ++){
+      if(d[i] < 48 || d[i] > 57){
+         cout << "not digit!";
+         flag = true;
+         break;
+      }
+   }
+   if(!flag){
+      cout << d ;
+   }
+   cout << endl;
    return 0;
 }
